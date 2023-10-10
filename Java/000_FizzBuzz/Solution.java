b7131f06876adbdd2af1a98475e934ae18d37a95
Akane IKENO <akane.ikeno@gmail.com>
2023-10-10 16:44:38 +0100

    commit

--- Java/000_FizzBuzz/Solution.java
+++ Java/000_FizzBuzz/Solution.java
@@ -4,7 +4,6 @@
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         /* Enter your code here.
            Print output to STDOUT.
            Your class should be named Solution.
